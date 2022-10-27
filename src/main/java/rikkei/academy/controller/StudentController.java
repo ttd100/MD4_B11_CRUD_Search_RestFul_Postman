a482e960b56cfe88efb8ae4df51d1f633c62103c
Your Name <dat054760@gmail.com>
2022-10-27 10:39:36 +0700

    up

---
+++ src/main/java/rikkei/academy/controller/StudentController.java
@@ -0,0 +1,80 @@
+package rikkei.academy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rikkei.academy.dto.response.ResponseMessage;
+import rikkei.academy.model.Student;
+import rikkei.academy.service.IStudentService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+    @Autowired
+    private IStudentService studentService;
+    @PostMapping
+    public ResponseEntity<?> createStudent(@RequestBody Student student) {
+        if (student.getName().trim().equals("")||student.getName().length()<3||student.getName().length()>10) {
+            return new ResponseEntity<>(new ResponseMessage("The name invalid"), HttpStatus.OK);
+        }
+        if (studentService.existsByName(student.getName())) {
+            return new ResponseEntity<>(new ResponseMessage("The name id existed"), HttpStatus.OK);
+        }
+        studentService.save(student);
+        return new ResponseEntity<>(new ResponseMessage("Created success!"),HttpStatus.OK);
+    }
+    @GetMapping ResponseEntity<?> showListStudent(){
+        return new ResponseEntity<>(studentService.findAll(),HttpStatus.OK);
+    }
+    @GetMapping("/page")
+    public ResponseEntity<?> pageStudent(@PageableDefault(sort = "name",size = 2) Pageable pageable){
+        return new ResponseEntity<>(studentService.findAll(pageable),HttpStatus.OK);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateStudent(@PathVariable Long id,@RequestBody Student student){
+        Optional<Student> student1 = studentService.findById(id);
+        if (!student1.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (student.getName().trim().equals("")||student.getName().length()<3||student.getName().length()>10){
+            return new ResponseEntity<>(new ResponseMessage("the name is invalid"),HttpStatus.OK);
+        }
+        if (studentService.existsByName(student.getName())){
+            return new ResponseEntity<>(new ResponseMessage("the name is existed"),HttpStatus.OK);
+        }
+        student1.get().setName(student.getName());
+        studentService.save(student1.get());
+        return new ResponseEntity<>(new ResponseMessage("Update success"),HttpStatus.OK);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<?>detailStudent(@PathVariable Long id){
+        Optional<Student> student = studentService.findById(id);
+        if(!student.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student.get(),HttpStatus.OK);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id){
+        Optional<Student> student = studentService.findById(id);
+        if (!student.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        studentService.deleteById(id);
+        return new ResponseEntity<>(new ResponseMessage("delete student success"),HttpStatus.OK);
+    }
+    // search @pathVariable
+    @GetMapping("/search/{name}")
+    public ResponseEntity<?> searchByName(@PathVariable String name){
+        return new ResponseEntity<>(studentService.findAllByNameContaining(name),HttpStatus.OK);
+    }
+    @GetMapping("/search/page")
+    public ResponseEntity<?> searchPageStudent(@RequestParam String name, Pageable pageable){
+        return new ResponseEntity<>(studentService.findAllByNameContaining(name,pageable),HttpStatus.OK);
+    }
+}
